7c98bf5802983561bd65ac839fb67111bf2fc7f9
tuhinhossain123 <csetuhin55@gmail.com>
2024-07-12 11:57:00 +0600

    dom connecte

---
+++ script.js
@@ -0,0 +1,10 @@
+// const allHeding = document.getElementsByTagName('h2');
+//     for(const h2 of allHeding){
+//         console.log(h2.innerHTML)
+//     }
+
+const allLi = document.getElementsByTagName("li");
+for (let i = 0; i <= allLi.length; i++) {
+  const index = allLi[i];
+  console.log(index);
+}
