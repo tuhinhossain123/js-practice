88e732fad47680c8b68284fc8decab6f74573f3b
tuhinhossain123 <csetuhin55@gmail.com>
2024-07-12 22:51:10 +0600

    innerHtml recap

--- script.js
+++ script.js
@@ -16,3 +16,26 @@ const liAdd = document.getElementById('li-list');
 const li = document.createElement("li");
 li.innerText="helo tuin";
 liAdd.appendChild(li)
+
+
+const mainContainer = document.getElementById('main-container');
+const sections = document.createElement('section');
+// sections.appendChild(mainContainer)
+const h1 = document.createElement('h1');
+h1.innerText='Furits Name';
+sections.appendChild(h1);
+const ul = document.createElement('ul');
+sections.appendChild(ul);
+const li1 = document.createElement('li');
+li1.innerText='Orange';
+ul.appendChild(li1);
+const li2 = document.createElement('li');
+li2.innerText='Mango';
+ul.appendChild(li2);
+const li3 = document.createElement('li');
+li3.innerText='Apple';
+ul.appendChild(li3);
+const li4 = document.createElement('li');
+li4.innerText='Watermelon';
+ul.appendChild(li4);
+mainContainer.appendChild(sections)
