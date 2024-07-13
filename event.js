50d1996f0f47a30d30614875e9871cbce585d2db
tuhinhossain123 <csetuhin55@gmail.com>
2024-07-13 14:37:41 +0600

    code recap

--- event.js
+++ event.js
@@ -1,8 +1,15 @@
-function makeRed(){
-document.body.style.backgroundColor='green'
+function makeRed() {
+  document.body.style.backgroundColor = "green";
 }
 
+document.getElementById("make-blue").addEventListener("click", function () {
+  document.body.style.backgroundColor = "blue";
+});
 
-document.getElementById('make-blue').addEventListener('click', function(){
-    document.body.style.backgroundColor='blue'
-})
+document.getElementById("submit-btn").addEventListener("click", function () {
+  const inputField = document.getElementById("input-field");
+  const inputText = inputField.value;
+  const p = document.getElementById("defult-text");
+  p.innerText = inputText;
+  inputField.value = ""
+});
