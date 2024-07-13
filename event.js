8d77c9fb84c5209ecbd06f0dffa8c61e7f265c1a
tuhinhossain123 <csetuhin55@gmail.com>
2024-07-13 15:04:12 +0600

    comment js recap

--- event.js
+++ event.js
@@ -13,3 +13,16 @@ document.getElementById("submit-btn").addEventListener("click", function () {
   p.innerText = inputText;
   inputField.value = ""
 });
+
+
+document.getElementById('cmnt-btn').addEventListener('click', function(){
+    const commentBox = document.getElementById('cmt-box');
+    const commentText = commentBox.value;
+
+    const commentArea = document.getElementById('cmnt-container');
+    const p = document.createElement('p');
+    p.innerText=commentText;
+    commentArea.appendChild(p);
+    commentBox.value = '';
+    console.log(commentText);
+})
